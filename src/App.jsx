63fb8ce4c72153116a8f93741e120299de0dd814
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2024-04-02 23:03:38 +0530

    init the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import {Button} from "@mui/material";tiyoo
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import DefaultTheme from "./theme/default-theme";
 import './App.css'
+import Home from './views/home.jsx';
 
 function App() {
   return (
     <ThemeProvider theme={DefaultTheme}>
-        <h1>Hello</h1>
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
+        <Home/>
     </ThemeProvider>
   )
 }
