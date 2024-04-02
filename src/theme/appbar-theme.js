2715f405c99383818cfcad5bfc24851cf04503e7
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2024-04-02 23:03:58 +0530

    init the project

---
+++ src/theme/appbar-theme.js
@@ -0,0 +1,54 @@
+import {createTheme} from "@mui/material";
+
+/*
+colors
+------
+main-green: #83a31a
+main-yellow: #FFDC2A
+secondary-green: #b9db2f
+main-gray: #edf2ff
+
+*/
+
+const DefaultTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#FFFFFF',
+        },
+        secondary: {
+            main: '#FFDC2A',
+        },
+        btnColor: {
+            white: '#b9db2f',
+            black: '#83a31a',
+        },
+        text: {
+            primary: '#000000'
+        },
+        subText: {
+            main: '#b9db2f'
+        }
+    },
+    typography: {
+        "fontFamily": `"Montserrat", "Helvetica", "Arial", sans-serif`,
+        "fontSize": 14,
+        "fontWeightLight": 300,
+        "fontWeightRegular": 400,
+        "fontWeightMedium": 500,
+        button: {
+            textTransform: 'none'
+        },
+        AppBar: {}
+    },
+    components: {
+        MuiTextField: {
+            styleOverrides: {
+                colorPrimary: {
+                    backgroundColor: '#edf2ff',
+                }
+            }
+        }
+    },
+});
+
+export default DefaultTheme;
