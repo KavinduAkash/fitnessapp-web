4dafceb23071cdb51d1bf80e445b2389bd0f5732
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2024-04-29 17:13:02 +0530

    dev post create model

--- src/components/create-post-bar.jsx
+++ src/components/create-post-bar.jsx
@@ -3,12 +3,12 @@ import Button from "@mui/material/Button";
 import "../assets/styles/create-post-bar.css";
 import PostAddIcon from '@mui/icons-material/PostAdd';
 
-function CreatePostBar() {
+function CreatePostBar(props) {
     return(
         <div className={'create-post-bar'}>
 
             <div></div>
-            <Button variant="contained"><PostAddIcon/>&nbsp;Create new post</Button>
+            <Button variant="contained" onClick={props.update}><PostAddIcon/>&nbsp;Create new post</Button>
 
         </div>
     )
--- src/views/home.jsx
+++ src/views/home.jsx
@@ -3,14 +3,101 @@ import Header from "../components/header.jsx";
 import "../assets/styles/home.css";
 import HomePostCard from "../components/home-post-card.jsx";
 import CreatePostBar from "../components/create-post-bar.jsx";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import UserVector from "../assets/user-vector.jpeg";
+import Button from "@mui/material/Button";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload.js";
+import {Box, FormControl, InputLabel, TextField} from "@mui/material";
+import DatePicker from "react-datepicker";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import DialogActions from "@mui/material/DialogActions";
+import Dialog from "@mui/material/Dialog";
+import ImageUploading from "react-images-uploading";
 
 function Home() {
+
+    const [open, setOpen] = React.useState(false);
+    const [scroll, setScroll] = React.useState('paper');
+
+    const [images, setImages] = React.useState([]);
+    const maxNumber = 3;
+    const onChange = (imageList, addUpdateIndex) => {
+        // data for submit
+        console.log(imageList, addUpdateIndex);
+        setImages(imageList);
+    };
+
+
+    const handleClose = () => {
+        setOpen(!open);
+    };
+
     return (
         <div>
+
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                scroll={scroll}
+                aria-labelledby="scroll-dialog-title"
+                aria-describedby="scroll-dialog-description"
+                minWidth={"lg"}
+            >
+                <DialogTitle id="scroll-dialog-title">Create new post</DialogTitle>
+                <DialogContent dividers={scroll === 'paper'}>
+                    <ImageUploading
+                        multiple
+                        value={images}
+                        onChange={onChange}
+                        maxNumber={maxNumber}
+                        dataURLKey="data_url"
+                        acceptType={["jpg"]}
+                    >
+                        {({
+                              imageList,
+                              onImageUpload,
+                              onImageRemoveAll,
+                              onImageUpdate,
+                              onImageRemove,
+                              isDragging,
+                              dragProps
+                          }) => (
+                            // write your building UI
+                            <div className="upload__image-wrapper">
+                                <button
+                                    style={isDragging ? { color: "red" } : null}
+                                    onClick={onImageUpload}
+                                    {...dragProps}
+                                >
+                                    Click or Drop here
+                                </button>
+                                &nbsp;
+                                <button onClick={onImageRemoveAll}>Remove all images</button>
+                                {imageList.map((image, index) => (
+                                    <div key={index} className="image-item">
+                                        <img src={image.data_url} alt="" width="100" />
+                                        <div className="image-item__btn-wrapper">
+                                            <button onClick={() => onImageUpdate(index)}>Update</button>
+                                            <button onClick={() => onImageRemove(index)}>Remove</button>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                    </ImageUploading>
+
+
+                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                </DialogContent>
+            </Dialog>
+
+
             <Header/>
             <section className={'home-content'}>
 
-                <CreatePostBar/>
+                <CreatePostBar update={handleClose}/>
 
                 <HomePostCard/>
                 <HomePostCard/>
